bc6e4297c157cd28c70bcaa8749efe57e47b3049
!shan <ishan210788@gmail.com>
2017-01-22 22:05:15 +0100

    adding gitignore

---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+* Path Contents: A module which takes target path as an argument and returns contents in the form of filesnames and directories
+* @author Ishan Tiwari <ishan210788@gmail.com>
+*/
+
+'use strict';
+let fs = require("fs");
+let path = require("path");
+
+let pathContent = [];
+let dirNames = [];
+let fileNames = [];
+
+/**
+   * A function that takes target path as an argument and returns contents in the form of filesnames and directories
+   * @param {string}      dir      A string which represents the target path
+   * @returns  {Array}    pathContent  An array object which has two arrayObjects, one for directory path and other for path of filenames.
+   */
+
+module.exports.getAllContents = function getContents(dir, filelist = []) {
+
+try {
+  fs.readdirSync(dir).forEach(file => {
+    if(fs.statSync(path.join(dir, file)).isDirectory()) {
+      dirNames.push(path.join(dir, file));
+      getContents(path.join(dir, file), filelist);
+    } else {
+      fileNames.push(path.join(dir, file));
+    }
+  });
+
+  pathContent.push({
+    'dirs': dirNames,
+    'files': fileNames
+  });
+
+  return pathContent[0];
+} catch(err) {
+    return new Error('Error occured: '+err);
+  }
+}
---
+++ test/test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var expect = require('chai').expect;
+var pathContents = require('../index');
+var mock = require('mock-fs');
+
+
+
+describe('#Check all files and folders', function() {
+
+/* Can also be done using mocks
+
+  mock({
+    'path/to/fake/dir': {
+      'some-file.txt': 'This is a testing file',
+      'foo': {/** empty directory}
+    },
+    'path/to/test.png': new Buffer([8, 6, 7, 5, 3, 0, 9]),
+    'some/other/path': {/** another empty directory }
+  });
+
+  */
+
+    let result = pathContents.getAllContents('test/foo');
+    console.log(result);
+
+    it('checking if there is any such dir', function() {
+      expect(result).to.not.equal(null);
+      //mock.restore();
+    }),
+
+    it('check all files', function() {
+      let countFiles = result.files.length;
+      expect(result.files.length).to.equal(3);
+      //mock.restore();
+    }),
+
+    it('check all the folder/subfolders', function() {
+      let countDirs = result.dirs.length;
+      expect(result.dirs.length).to.equal(3);
+      //mock.restore();
+    })
+
+  });
